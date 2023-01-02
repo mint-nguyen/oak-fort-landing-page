638c59c48c6a81781c1073d63da8271ecc44563d
Mint <mintnguyen.pa@gmail.com>
2023-01-02 15:53:19 -0800

    feat: banner comp

---
+++ components/Banner/index.tsx
@@ -0,0 +1,13 @@
+const Banner = () => {
+	return (
+		<div className="bg-black h-10 flex items-center justify-center top-0">
+			<p className="ml-3 truncate font-medium text-white text-sm">
+				<span className="hidden md:inline">
+					LAST CHANCE EVENT: UP TO 60% OFF
+				</span>
+			</p>
+		</div>
+	);
+};
+
+export default Banner;
